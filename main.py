428a880e46c23b866a2e65b8b8af1ca471e801fc
VladislavKulik <vladik.joker92@gmail.com>
2023-05-29 02:31:41 +0300

    im back

--- main.py
+++ main.py
@@ -29,47 +29,48 @@ headers = {
 }
 
 
-# def download_media(url):
-#     request_url = f'https://api.douyin.wtf/api?url={url}'
-#     response = requests.get(request_url, headers=headers)
-#     video_link = response.json()['video_data']['nwm_video_url_HQ']
-#     return video_link
+def download_media(url):
+    request_url = f'https://api.douyin.wtf/api?url={url}'
+    response = requests.get(request_url, headers=headers)
+    video_link = response.json()['video_data']['nwm_video_url_HQ']
+    return video_link
 
 
 @dp.message_handler(commands=['start', 'Start'])
 async def send_welcome(message: types.Message):
     await message.reply(f'Бот работает, скиньте ссылку на ТикТок')
+    await download.name.set()
 
 
-# @dp.message_handler(commands=['help', 'Help'])
-# async def send_help(message: types.Message):
-#     await download.name.set()
-#     await message.reply(f'Бог поможет')
+@dp.message_handler(commands=['help', 'Help'])
+async def send_help(message: types.Message):
+    # await download.name.set()
+    await message.reply(f'Бог поможет')
 
 
-# @dp.message_handler(commands=['rules', 'Rules'])
-# async def send_rules(message: types.Message):
-#     await download.name.set()
-#     await message.reply(f'Правила бота: \n' f'1. Без алкоголя\n' f'2. Без оскорблений\n' f'3. Без доты\n' f'4. Адекватность приветствуется')
+@dp.message_handler(commands=['rules', 'Rules'])
+async def send_rules(message: types.Message):
+    # await download.name.set()
+    await message.reply(f'Правила бота: \n' f'1. Без алкоголя\n' f'2. Без оскорблений\n' f'3. Без доты\n' f'4. Адекватность приветствуется')
 
 
-@dp.message_handler(commands=['analyzer', 'Analyzer'])
-async def send_analyzer(message: types.Message):
-    await message.reply(f'Сентимент: негативний \n' f'Кількість позитивних слів: 0 \n' f'Кількість негативних слів: 2\n'f'Кількість нейтральних слів: 4\n'f'Кількість загальних слів: 9\n')
+# @dp.message_handler(commands=['analyzer', 'Analyzer'])
+# async def send_analyzer(message: types.Message):
+#     await message.reply(f'Сентимент: негативний \n' f'Кількість позитивних слів: 0 \n' f'Кількість негативних слів: 2\n'f'Кількість нейтральних слів: 4\n'f'Кількість загальних слів: 9\n')
 
 
-# @dp.message_handler(state=download.name)
-# async def process_name(message: types.Message, state: FSMContext):
-#     await state.finish()
-#     if re.compile('https://[a-zA-Z]+.tiktok.com/').match(message.text):
-#         link = download_media(message.text)
-#         caption = hlink("Ссылка", message.text), hlink(
-#             message.from_user.username, "tg://user?id="+str(message.from_user.id)+"")
-#         await message.reply_video(link, caption=caption, parse_mode=ParseMode.HTML)
-#         await message.delete()
-#         return await download.name.set()
-#     else:
-#         await message.answer('Походу Бот сломался (может и нет). Почему он сломался, я не знаю.')
+@dp.message_handler(state=download.name)
+async def process_name(message: types.Message, state: FSMContext):
+    await state.finish()
+    if re.compile('https://[a-zA-Z]+.tiktok.com/').match(message.text):
+        link = download_media(message.text)
+        caption = hlink("Ссылка", message.text), hlink(
+            message.from_user.username, "tg://user?id="+str(message.from_user.id)+"")
+        await message.reply_video(link, caption=caption, parse_mode=ParseMode.HTML)
+        # await message.delete()
+        return await download.name.set()
+    else:
+        await message.answer('Походу Бот сломался (может и нет). Почему он сломался, я не знаю.')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
